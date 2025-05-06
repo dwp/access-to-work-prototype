660ff9e2baa149fe7ebd2a3a0e9314fc41e9fe5e
Matthewrhys37 <107192169+Matthewrhys37@users.noreply.github.com>
2025-05-06 11:45:48 +0100

    Create prototype
    GitOrigin-RevId: f70213322914b4160d70b5d8c9f53e634b8b57b3

--- app/routes.js
+++ app/routes.js
@@ -7,24 +7,3 @@ const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
 
 // Add your routes here
-
-router.post('/referral-type-answer', function(request, response) {
-
-    var referralType = request.session.data['send-new-referral']
-    if (referralType == "Workplace Assessment"){
-        response.redirect("/v1/workplace-assessment/personal-details")
-    } else {
-        response.redirect("/v1/mental-health-service/personal-details")
-    }
-})
-
-router.post('/referral-type-mhs-answer', function(request, response) {
-
-    var referralType = request.session.data['referral-type']
-    if (referralType == "Authority referral"){
-        response.redirect("/v1/mental-health-service/provider-details-authority")
-    } else {
-        response.redirect("/v1/mental-health-service/provider-details-self-referral")
-    }
-})
-
