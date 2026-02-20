591ec44a523b84d8481d1d76fb73fc21a0091ee5
JamesCrisp1 <james.crisp1@engineering.digital.dwp.gov.uk>
2026-02-20 09:58:55 +0000

    Jc atwis nino
    GitOrigin-RevId: 4960463a67a71a24ddaacbffdfc831aa996e4d87

--- app/filters.js
+++ app/filters.js
@@ -5,6 +5,31 @@
 
 const govukPrototypeKit = require('govuk-prototype-kit')
 const addFilter = govukPrototypeKit.views.addFilter
-
+const moment = require('moment')
 // Add your filters here
+
+addFilter('formatDate', function(dateString){
+
+	return moment(dateString).format("D MMMM YYYY")
+})
+
+
+
+
+addFilter('uniqueTypes', function (arr) {
+  if (!Array.isArray(arr)) return arr;
+
+  const seen = new Set();
+
+  return arr.filter(item => {
+    if (!item || !item.type) return false;
+
+    if (seen.has(item.type)) {
+      return false;
+    }
+
+    seen.add(item.type);
+    return true;
+  });
+});
 
