0e8e75c33bae12071c685dcecb7646e0939be7fe
Emilia Bromby <emilia.bromby@engineering.digital.dwp.gov.uk>
2026-03-24 09:13:39 +0000

    Awtis snag list
    GitOrigin-RevId: 5b98d1ccbb0d132d1372ef084de219be5656e2dc

---
+++ app/views/atwis/v5/data/unallocated-documents.js
@@ -0,0 +1,213 @@
+module.exports = [{
+  "id": 1,
+  "name": "Shermy Franzini",
+  "documents": [
+    {
+      "dateScanned": "2026-02-07",
+      "id": "feb024ff-4d79-4209-840d-07c60a052eab",
+      "formReference": "DZ601ZN",
+      "type": "Support Worker (multiple)"
+    },
+    {
+      "dateScanned": "2026-02-03",
+      "id": "95344a1b-2a5b-4984-9e42-a9c85437aa5a",
+      "formReference": "QY830KA",
+      "type": "Change bank details"
+    }
+  ]
+}, {
+  "id": 2,
+  "name": "Frances Lamball",
+  "documents": [
+    {
+      "dateScanned": "2026-02-10",
+      "id": "aa5afd6f-92c0-4863-b210-ab96940daf21",
+      "formReference": "ZY930WZ",
+      "type": "Support Worker"
+    }
+  ]
+}, {
+  "id": 3,
+  "name": "Lorri Facchini",
+  "documents": [
+    {
+      "dateScanned": "2026-01-21",
+      "id": "94efeed4-8be9-48c9-a919-4b80c102e90f",
+      "formReference": "WM928ZT",
+      "type": "Travel to work"
+    },
+    {
+      "dateScanned": "2026-01-28",
+      "id": "8586686a-ded2-421a-b9d6-c8f38ad0f703",
+      "formReference": "UL960NQ",
+      "type": "Support Worker"
+    },
+    {
+      "dateScanned": "2026-01-08",
+      "id": "5743e44b-b891-4913-88e4-f1d6c07d6d34",
+      "formReference": "JH513RL",
+      "type": "One off costs (SAE)"
+    }
+  ]
+}, {
+  "id": 4,
+  "name": "Sheff Algeo",
+  "documents": [
+    {
+      "dateScanned": "2026-01-16",
+      "id": "ebcbfb27-d444-4467-8b08-b90f8dd73254",
+      "formReference": "BK868WP",
+      "type": "CSI"
+    },
+    {
+      "dateScanned": "2026-01-16",
+      "id": "9a4f5b3f-4536-4273-93cb-26104827d7a0",
+      "formReference": "JM450IR",
+      "type": "One off costs (SAE)"
+    },
+    {
+      "dateScanned": "2026-02-06",
+      "id": "13323968-f1c9-40ba-b3f3-8e444248ac2d",
+      "formReference": "LS447PX",
+      "type": "Change bank details"
+    },
+    {
+      "dateScanned": "2026-01-15",
+      "id": "2a8bc77c-ce3f-4606-a48d-7d77c6f92a9c",
+      "formReference": "VH251WI",
+      "type": "Travel in work"
+    }
+  ]
+}, {
+  "id": 5,
+  "name": "Caz Ellerbeck",
+  "documents": [
+    {
+      "dateScanned": "2026-01-25",
+      "id": "f43bf8b4-e136-4950-85da-9d7e3dc8a4fa",
+      "formReference": "XK029MY",
+      "type": "One off costs (SAE)"
+    },
+    {
+      "dateScanned": "2026-02-01",
+      "id": "4491ffda-f7c6-4b19-b611-0e3c48f98663",
+      "formReference": "IL382HZ",
+      "type": "One off costs (SAE)"
+    }
+  ]
+}, {
+  "id": 6,
+  "name": "Noak Veljes",
+  "documents": [
+    {
+      "dateScanned": "2026-01-30",
+      "id": "04e46b1c-9475-4c3f-a84f-0db6f0315d28",
+      "formReference": "IX814IS",
+      "type": "Support Worker"
+    },
+    {
+      "dateScanned": "2026-01-04",
+      "id": "819a21e9-662d-4421-abfc-2a11073f5939",
+      "formReference": "KC908ZS",
+      "type": "One off costs (SAE)"
+    }
+  ]
+}, {
+  "id": 7,
+  "name": "Zara Huitson",
+  "documents": [
+    {
+      "dateScanned": "2026-02-04",
+      "id": "35081869-0a2b-4f51-8a90-ad2689a94601",
+      "formReference": "UP064GO",
+      "type": "Travel to work"
+    },
+    {
+      "dateScanned": "2026-01-27",
+      "id": "9f5f496f-79f1-425a-bbd3-d164e721cae2",
+      "formReference": "OB982GL",
+      "type": "CSI"
+    },
+    {
+      "dateScanned": "2026-01-25",
+      "id": "a5c6fc63-9d3b-40e8-8451-dda1151d0f76",
+      "formReference": "KZ760JO",
+      "type": "Support Worker (multiple)"
+    },
+    {
+      "dateScanned": "2026-01-18",
+      "id": "19917a4b-18b6-4ff3-b2aa-24e1052ed343",
+      "formReference": "JH113ZS",
+      "type": "Travel in work"
+    },
+    {
+      "dateScanned": "2026-02-10",
+      "id": "5a3217e3-8886-40e0-913c-d93187a61572",
+      "formReference": "YJ549LR",
+      "type": "Support Worker (multiple)"
+    }
+  ]
+}, {
+  "id": 8,
+  "name": "Conn Toward",
+  "documents": [
+    {
+      "dateScanned": "2026-02-11",
+      "id": "7a944e22-dfbd-4bf4-8f37-2c6842546075",
+      "formReference": "NJ329HQ",
+      "type": "Travel in work"
+    },
+    {
+      "dateScanned": "2026-01-19",
+      "id": "32ca3929-4667-4da6-90ff-c3b773411555",
+      "formReference": "GZ867EL",
+      "type": "Travel to work"
+    },
+    {
+      "dateScanned": "2026-01-17",
+      "id": "19652b7d-ce98-4b84-b8a1-9c827c337bf7",
+      "formReference": "RC987DJ",
+      "type": "One off costs (SAE)"
+    }
+  ]
+}, {
+  "id": 9,
+  "name": "Ivar Bagenal",
+  "documents": [
+    {
+      "dateScanned": "2026-01-14",
+      "id": "90cc24bc-373d-4fd8-bff1-7f5a0e29b501",
+      "formReference": "PV863JZ",
+      "type": "Support Worker (multiple)"
+    },
+    {
+      "dateScanned": "2026-01-07",
+      "id": "ec799400-c451-4ef5-b585-df2b7d32267e",
+      "formReference": "GF323FG",
+      "type": "One off costs (SAE)"
+    }
+  ]
+}, {
+  "id": 10,
+  "name": "Karlan Blinder",
+  "documents": [
+    {
+      "dateScanned": "2026-01-24",
+      "id": "602edf9a-a8d9-4dca-8361-7588fcdbb089",
+      "formReference": "RX308XE",
+      "type": "Travel to work"
+    },
+    {
+      "dateScanned": "2026-01-18",
+      "id": "d70a3eb4-afc7-403a-869c-1e20a39025ee",
+      "formReference": "VF530QF",
+      "type": "Travel in work"
+    },
+    {
+      "dateScanned": "2026-01-12",
+      "id": "41680197-da6a-4a81-a5a5-93d80d7db70d",
+      "formReference": "AX510AQ",
+      "type": "Change bank details"
+    }
+  ]
+}]
