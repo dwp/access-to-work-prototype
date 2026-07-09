61795b3760d6c874207d4e5ea4cc4450fd1f2b1a
EmiliaBromby <emilia.bromby@engineering.digital.dwp.gov.uk>
2026-07-09 15:21:57 +0100

    Self-employment - Director specific
    GitOrigin-RevId: 751b15638cd3bdc7344253a761d313a80320b2bb

--- app/views/continuous-improvement/apply/v3/_routes.js
+++ app/views/continuous-improvement/apply/v3/_routes.js
@@ -340,9 +340,9 @@ router.post(versionPath + '/support-other-jobs-answer', function(req, res) {
         if (jobStatus == "Employed"){
             res.redirect(versionPath + "/job/starting-job/employed/company")
         } else if (jobStatus == "Self-employed"){
-            res.redirect(versionPath + "/job/starting-job/self-employed/guidance")
+            res.redirect(versionPath + "/job/starting-job/self-employed/job-title")
         } else if (jobStatus == "Registered director"){
-            res.redirect(versionPath + "/job/starting-job/registered-director/guidance")
+            res.redirect(versionPath + "/job/starting-job/registered-director/job-title")
         }
     })
 
