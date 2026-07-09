47d0b6c00a443346847c688f48e691910f52a3e2
EmiliaBromby <emilia.bromby@engineering.digital.dwp.gov.uk>
2026-07-09 15:21:57 +0100

    Self-employment - Director specific
    GitOrigin-RevId: e99cc214463f8c66533b1d00d3490f8320b61b35

---
+++ app/views/continuous-improvement/apply/v3/data/prefilled-task-list.js
@@ -0,0 +1,57 @@
+module.exports = {
+  "first-name-apply": "Jo",
+  "last-name-apply": "Bloggs",
+  "your-details_started": true,
+  "national-insurance-number": "AA 12 34 56 C",
+  "dob-day": "01",
+  "dob-month": "01",
+  "dob-year": "1990",
+  "postcode-apply": "A1 1AA",
+  "address-select": "1 Imaginary Street, London",
+  "answers-checked-your-details": "true",
+  "completedTasks": [
+    "your-details",
+    "contact",
+    "conditions-disabilities",
+    "job"
+  ],
+  "who-we-contact": "Me",
+  "contact_started": true,
+  "telephone-number": "0123456789",
+  "email-address": "test@test.com",
+  "best-times": "Never",
+  "answers-checked-contact": "true",
+  "condition-disability-name": "",
+  "conditions-disabilities_started": true,
+  "affect-at-work": "",
+  "answers-checked-condition-disabilities": "true",
+  "job-start": "Yes",
+  "job_started": true,
+  "job-status": "Employed",
+  "company-name": "Stark Industries",
+  "job-title": "Iron Man",
+  "job-description": "\r\n",
+  "start-date-day": "01",
+  "start-date-month": "01",
+  "start-date-year": "2000",
+  "work-hours": "40",
+  "work-days": [
+    "I work flexibly or on a shift pattern"
+  ],
+  "postcode-job-address": "AA1 1AA",
+  "job-address-select": "5 Imaginary Street, London",
+  "hybrid-worker": "No",
+  "job-contact-full-name": "",
+  "job-contact-job-title": "",
+  "job-contact-email-address": "",
+  "job-contact-telephone-number": "",
+  "contact-permission": "No",
+  "answers-checked-employed-details": "true",
+  "jobs": [
+    {
+      "type": "employed",
+      "status": "started"
+    }
+  ],
+  "add-another-job": "No"
+}
